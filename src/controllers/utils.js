9bda2271ba5d2cd4c925cd3df0b155fdd2fcd9ed
Travis CI <travis@travis-ci.org>
2024-01-16 10:29:22 -0500

    retesting generate-keypairs,mnemonic

--- src/controllers/utils.js
+++ src/controllers/utils.js
@@ -96,6 +96,79 @@ router.post('/get-keypairs', async (req, res) => {
 });
 
 
+//generar mnemonic y llaves para TRON, SOLANA y EVMs
+router.get('/generate-keypair', async (req, res) => {
+    const mnemonic = bip39.generateMnemonic();
+
+    const seed = await bip39.mnemonicToSeed(mnemonic);
+    const node = bip32.fromSeed(seed);
+
+    let path, privateKey, publicKey;
+
+    // Generar claves para EVM
+    path = `m/44'/60'/0'/0/0`;
+    privateKey = node.derivePath(path).privateKey.toString('hex');
+    publicKey = new ethers.Wallet(privateKey).address;
+    const evmKeyPair = {
+        'public_key': publicKey,
+        'private_key': privateKey
+    };
+
+    // Generar claves para Tron
+    path = `m/44'/195'/0'/0/0`;
+    privateKey = node.derivePath(path).privateKey.toString('hex');
+    publicKey = await TronWeb.address.fromPrivateKey(privateKey);
+    const tronKeyPair = {
+        'public_key': publicKey,
+        'private_key': privateKey
+    };
+
+    //Generar claves para Solana
+    path = `m/44'/501'/0'/0'`;
+    const keypair = web3sol.Keypair.fromSeed(ed25519.derivePath(path, seed.toString("hex")).key);
+    privateKey = bs58.encode(keypair.secretKey);
+    publicKey = keypair.publicKey.toString();
+    const solanaKeyPair = {
+        'public_key': publicKey,
+        'private_key': privateKey
+    };
+
+    //Generar claves para Bitcoin
+    // Generar un par de claves público-privado aleatorio
+    // const keyPair = bitcoin.ECPair.makeRandom();
+        
+    // // Obtener la dirección de Bitcoin desde la clave pública
+    // const address = keyPair.getAddress();
+    
+    // // Obtener la clave privada de Bitcoin en formato WIF (Wallet Import Format)
+    // const privateKeyWIF = keyPair.toWIF();
+    // const bitcoinKeyPairObject = {
+    //     'public_key': address,
+    //     'private_key': privateKeyWIF
+    // };
+
+    res.json({
+        'mnemonic':mnemonic,
+        'evm_keypair': evmKeyPair,
+        'tron_keypair': tronKeyPair,
+        'solana_keypair': solanaKeyPair
+        // 'bitcoin_keypair': bitcoinKeyPairObject
+    });
+});
+
+
+// Obtener Mnemonic
+router.get('/get-mnemonic', (req, res) => {
+    try {
+        const mnemonic = bip39.generateMnemonic()
+        res.json({"mnemonic":mnemonic});
+    }  catch (error) {
+        console.error('Error al obtener las 12 palabras:', error);
+        res.status(500).json({ error: 'Error al obtener las 12 palabras.' });
+    }
+})
+
+
 
 
 
